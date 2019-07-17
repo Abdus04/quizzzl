8b7a148cdab4a1e1624132b512c5eed1ebb81377
Abdus04 <44335984+Abdus04@users.noreply.github.com>
2019-07-17 22:11:44 +0100

    Add files via upload

---
+++ AcceuilFormateur.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AcceuilFormateur {
+
+
+    public void deconnecter(ActionEvent event) throws IOException
+    {
+
+
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("QuizZzl.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(tableViewScene);
+        window.show();
+    }
+
+}
+
